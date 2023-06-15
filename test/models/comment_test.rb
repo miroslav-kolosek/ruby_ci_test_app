19c4b8b421a7df9922d1ac7dacdc8622a052cfbf
miroslav <miroslav@kolosek.com>
2023-06-15 09:47:22 +0200

    minitest tests 1

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "the false" do
+    assert_not true
+  end
 end
